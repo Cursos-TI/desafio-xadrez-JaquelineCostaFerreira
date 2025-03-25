0baf2ed085ad03a0326eb25b4129745c80758398
Jaqueline Costa Ferreira Pereira <jaquecostafp@gmail.com>
2025-03-25 17:57:40 +0000

    Adicionei o movimento do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main() {
     // Movimento da Torre - usando for
     int casas_torre = 5;
@@ -26,6 +27,20 @@ int main() {
         printf("Esquerda\n");
         contador_rainha++;
     } while (contador_rainha < casas_rainha);
+    printf("\n");
+
+    // Movimento do Cavalo - usando for e while
+    printf("Movimento do Cavalo:\n");
+    int i;
+    for (i = 0; i < 2; i++) { // Duas casas para baixo
+        printf("Baixo\n");
+    }
+    int j = 0;
+    while (j < 1) { // Uma casa para a esquerda
+        printf("Esquerda\n");
+        j++;
+    }
 
     return 0;
 }
+
